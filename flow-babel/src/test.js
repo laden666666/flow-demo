78ed2a53d2a08b1163c6cb39eccd79447e620299
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2017-06-21 07:41:24 +0800

    提交示例

---
+++ flow-babel/src/test.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+function foo(x){
+    return x * 12;     //Flow根据代码，推断foo的x1参数是string类型，string类型不能执行“*”操作，所有这里Flow会报错
+}
+
+foo("Hello, world!");
+
+let a : number = '';
---
+++ flow-demo/src/test.js
@@ -0,0 +1,7 @@
+/* @flow */
+
+function foo(x1){
+    return x1 * 12;     //Flow根据代码，推断foo的x1参数是string类型，string类型不能执行“*”操作，所有这里Flow会报错
+}
+
+foo("Hello, world!");
